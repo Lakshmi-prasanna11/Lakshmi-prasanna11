a2407b8758e558185a82f2f208511667a3ef4f10
Lakshmi Prasanna Ramisetty <80476865+Lakshmi-prasanna11@users.noreply.github.com>
2024-01-09 22:56:07 +0530

    Add files via upload

---
+++ Portfolio/contact.js
@@ -0,0 +1,15 @@
+function mylocation(){
+    window.location="https://maps.app.goo.gl/jQwrL6sinc5fjXG96"
+}
+function instagram(){
+    window.location="https://www.instagram.com/lakshmiprasannaramisetty/"
+}
+function linkedin(){
+    window.location="https://www.linkedin.com/in/lakshmi-prasanna-ramisetty-b91a08205/?originalSubdomain=in"
+}
+function facebook(){
+    window.location="https://www.facebook.com/lakshmiprasanna.ramisetty.5/"
+}
+function mail(){
+    window.location="https://mail.google.com/mail/u/0/#inbox"
+}
